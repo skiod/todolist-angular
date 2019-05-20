ef0122fff3d39f8df33ab6b08fc6b562f00eaac3
khalid <khalid94.skiod@gmail.com>
2019-05-20 00:53:41 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+  name  = "to do list by khalid skiod";
+  inputText =  "";
+  items = []
+
+  add(){
+    this.items.push(this.inputText)
+    this.inputText = ""
+  }
+  delete(index){
+    this.items.splice(index,1);
+  }
 }
